8c3c755b89b8608211f47d488ea4e18b48174d41
Tibor Kircsi <tkircsi@gmail.com>
2020-05-05 23:24:34 +0200

    file upload to S3

--- config/ci.js
+++ config/ci.js
@@ -4,4 +4,6 @@ module.exports = {
   mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
   cookieKey: '123123123',
   redisUrl: 'redis://127.0.0.1:6379',
+  accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+  secretAccessKey: process.env.AWS_S3_SECRET_KEY,
 };
